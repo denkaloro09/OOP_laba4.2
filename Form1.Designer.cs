00e2a641db09a082eda6b4fca5add5452d43b6c7
Denis <denqwadr2019@gmail.com>
2021-11-26 21:02:39 +0500

    доделал необходимое поведение

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Laba4_2oop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.tbar1 = new System.Windows.Forms.TrackBar();
+            this.tbar2 = new System.Windows.Forms.TrackBar();
+            this.tbar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(37, 56);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(120, 20);
+            this.tb1.TabIndex = 0;
+            this.tb1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb1_KeyDown);
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(188, 56);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(120, 20);
+            this.tb2.TabIndex = 1;
+            this.tb2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb2_KeyDown);
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(342, 56);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(120, 20);
+            this.tb3.TabIndex = 2;
+            this.tb3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb3_KeyDown);
+            // 
+            // nud1
+            // 
+            this.nud1.Location = new System.Drawing.Point(37, 93);
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(120, 20);
+            this.nud1.TabIndex = 3;
+            this.nud1.ValueChanged += new System.EventHandler(this.nud1_ValueChanged);
+            // 
+            // nud2
+            // 
+            this.nud2.Location = new System.Drawing.Point(188, 92);
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(120, 20);
+            this.nud2.TabIndex = 4;
+            this.nud2.ValueChanged += new System.EventHandler(this.nud2_ValueChanged);
+            // 
+            // nud3
+            // 
+            this.nud3.Location = new System.Drawing.Point(342, 92);
+            this.nud3.Name = "nud3";
+            this.nud3.Size = new System.Drawing.Size(120, 20);
+            this.nud3.TabIndex = 5;
+            this.nud3.ValueChanged += new System.EventHandler(this.nud3_ValueChanged);
+            // 
+            // tbar1
+            // 
+            this.tbar1.Location = new System.Drawing.Point(37, 120);
+            this.tbar1.Maximum = 100;
+            this.tbar1.Name = "tbar1";
+            this.tbar1.Size = new System.Drawing.Size(120, 45);
+            this.tbar1.TabIndex = 6;
+            this.tbar1.Scroll += new System.EventHandler(this.tbar1_Scroll);
+            // 
+            // tbar2
+            // 
+            this.tbar2.Location = new System.Drawing.Point(188, 119);
+            this.tbar2.Maximum = 100;
+            this.tbar2.Name = "tbar2";
+            this.tbar2.Size = new System.Drawing.Size(120, 45);
+            this.tbar2.TabIndex = 7;
+            this.tbar2.Scroll += new System.EventHandler(this.tbar2_Scroll);
+            // 
+            // tbar3
+            // 
+            this.tbar3.Location = new System.Drawing.Point(342, 119);
+            this.tbar3.Maximum = 100;
+            this.tbar3.Name = "tbar3";
+            this.tbar3.Size = new System.Drawing.Size(120, 45);
+            this.tbar3.TabIndex = 8;
+            this.tbar3.Scroll += new System.EventHandler(this.tbar3_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 243);
+            this.Controls.Add(this.tbar3);
+            this.Controls.Add(this.tbar2);
+            this.Controls.Add(this.tbar1);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud1);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.TrackBar tbar1;
+        private System.Windows.Forms.TrackBar tbar2;
+        private System.Windows.Forms.TrackBar tbar3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba4_2oop
+{
+    public partial class Form1 : Form
+    {
+        Model model;       
+        public Form1()
+        {
+            InitializeComponent();            
+            model = new Model();           
+            model.observers += new System.EventHandler(this.UpdateFromModel);
+        }        
+        private void UpdateFromModel(object sender, EventArgs e) 
+        {
+            //для А и С
+            tb1.Text = model.getValueA().ToString();
+            nud1.Value = model.getValueA();
+            tbar1.Value = model.getValueA();
+            tb3.Text = model.getValueC().ToString();
+            nud3.Value = model.getValueC();
+            tbar3.Value = model.getValueC();
+            //для B
+            tb2.Text = model.getValueB().ToString();
+            nud2.Value = model.getValueB();
+            tbar2.Value = model.getValueB();
+        }
+
+        private void nud1_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValueA(Decimal.ToInt32(nud1.Value));
+        }
+
+        private void tbar1_Scroll(object sender, EventArgs e)
+        {
+            model.setValueA(tbar1.Value);
+        }
+
+        private void tb1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                model.setValueA(Int32.Parse(tb1.Text));
+        }
+
+        private void tb3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                model.setValueC(Int32.Parse(tb3.Text));
+        }
+
+        private void nud3_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValueC(Decimal.ToInt32(nud3.Value));
+        }
+
+        private void tbar3_Scroll(object sender, EventArgs e)
+        {
+            model.setValueC(tbar3.Value);
+        }
+
+        private void nud2_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValueB(Decimal.ToInt32(nud2.Value));
+        }
+
+        private void tb2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                model.setValueB(Int32.Parse(tb2.Text));
+        }
+
+        private void tbar2_Scroll(object sender, EventArgs e)
+        {
+            model.setValueB(tbar2.Value);
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Properties.Settings.Default.valueA = model.getValueA();
+            Properties.Settings.Default.valueB = model.getValueB();
+            Properties.Settings.Default.valueC = model.getValueC();
+            Properties.Settings.Default.Save();
+        }
+        private void LoadSettings()
+        {
+            tb1.Text = Properties.Settings.Default.valueA.ToString();
+            nud1.Value = Properties.Settings.Default.valueA;
+            tbar1.Value = Properties.Settings.Default.valueA;            
+            tb3.Text = Properties.Settings.Default.valueC.ToString();                    
+            nud3.Value = Properties.Settings.Default.valueC;                  
+            tbar3.Value = Properties.Settings.Default.valueC;
+            tb2.Text = Properties.Settings.Default.valueB.ToString();
+            nud2.Value = Properties.Settings.Default.valueB;
+            tbar2.Value = Properties.Settings.Default.valueB;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSettings();
+        }
+    }
+    public class Model
+    {
+        private int valueA;
+        private int valueB;
+        private int valueC;
+        public System.EventHandler observers;
+        public int getValueA() 
+        {
+            return valueA;
+        }
+        public int getValueB()
+        {
+            return valueB;
+        }
+        public int getValueC()
+        {
+            return valueC;
+        }
+        public void setValueA(int _value) 
+        {
+            if (_value > 0 && _value < 100) //если _value от нуля до ста
+            {
+                if (_value <= valueC) //и меньше или равно C
+                {
+                    valueA = _value; //тогда А равно _value
+                    if (valueB < _value) //если В меньше _value
+                    {
+                        valueB = _value; //тогда В равно _value
+                    }
+                }
+                else //если _value больше С
+                {
+                    valueA = _value; //тогда A равно _value
+                    valueC = _value; // и С
+                    valueB = _value; // и B
+                }
+            } 
+            else if (_value > 100) //если же _value больше ста
+            {
+
+                valueA = 100; //тогда наверное так
+                valueC = 100;
+                valueB = 100;
+            }
+            else if (_value < 0) //если меньше нуля
+            {
+                valueA = 0; //тогда А = 0
+            }
+            observers.Invoke(this,null);
+        }
+        public void setValueC(int _value)
+        {
+            if (_value > 0 && _value < 100) //если от нуля до ста
+            {
+                if (_value >= valueA) //и если больше А
+                {
+                    valueC = _value; //тогда равно
+                    if (valueB > _value) //если В больше _value
+                    {
+                        valueB = _value; //тогда В равно _value
+                    }
+                }
+                else //если меньше А
+                {
+                    valueC = _value; //тогда равно
+                    valueA = _value; // и это
+                    valueB = _value; // и это
+                }
+                
+            }
+            else if (_value > 100)
+            {
+                valueC = 100;
+            }
+            else if (_value < 0)
+            {
+                valueC = 0;
+                valueB = 0;
+                valueA = 0;
+            }
+            observers.Invoke(this, null);
+        }
+        public void setValueB(int _value)
+        {
+            if (_value >= valueA && _value <= valueC) //если больше или равно А и меньше или равно С
+            {
+                valueB = _value; //тогда все норм 
+            }
+            else if (_value > valueC) //если больше С
+            {
+                //valueB = valueC;
+            }
+            else if (_value < valueA) //если меньше А
+            {
+                //valueB = valueA;
+            }         
+            observers.Invoke(this, null);
+        }
+    }
+}
